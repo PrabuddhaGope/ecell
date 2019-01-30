f3f28850ede6dabca138c227ac038425e89e8816
Sagar Bansal <sagarbansal099@gmail.com>
2019-01-30 18:02:22 +0000

    tee2

---
+++ payment-success.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <!-- Begin Head -->
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125403862-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-125403862-1');
+        </script>
+
+        <!-- Basic -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>E-CELL | VNIT</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="E-Cell VNIT aims to promote and develop entrepreneurship culture amongst the students. Throughout the year, we
+                        conduct various events, workshops and speaker
+                        sessions sessions for inspiring and assisting young
+                        entrepreneurs in their journey in the world of
+                        business">
+        <meta name="author" content="Sagar Bansal">
+
+        <!-- Web Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet">
+
+        <!-- Vendor Styles -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/animate.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/themify/themify.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/scrollbar/scrollbar.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme Styles -->
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/overlay.css" rel="stylesheet" type="text/css"/>
+        <link href="css/global/global.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/icon.png">
+    </head>
+    <!-- End Head -->
+
+    <!-- Body -->
+    <body>
+
+        <!--========== HEADER ==========-->
+
+        <!--========== END HEADER ==========-->
+
+        <!--========== PROMO BLOCK ==========-->
+        <div class="g-bg-position--center js__parallax-window" style="background: black 100% 0 no-repeat fixed; height:100vh;">
+            <div class="g-container--md g-text-center--xs g-padding-y-40--xs ">
+                <h1 class="g-font-size-20--xs g-font-size-40--sm g-font-size-50--md g-color--white g-letter-spacing--3">THANK YOU | ECELLVNIT</h1>
+
+                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--white-opacity g-letter-spacing--2 g-margin-b-25--xs"><?php
+                    if(isset($_POST['txnid'])){
+                        echo "Your Transaction ID: ".$_POST['txnid'];
+                    }
+
+                ?></p>
+
+
+                <div class="store-img g-padding-y-60--xs g-padding-y-60--md">
+                    <img class="img-1" src="img/store/1_white-01.png" alt="" >
+                    <img class="img-2" src="img/store/1_red-01.png" alt="">
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        <!--========== END PROMO BLOCK ==========-->
+
+
+        <!-- End Speakers -->
+
+                <!--========== END PAGE CONTENT ==========-->
+
+        <!--========== FOOTER ==========-->
+                <footer class="g-bg-color--dark">
+
+            <!-- End Sponsors -->
+            <!-- Copyright -->
+            <div class="container g-padding-y-20--xs">
+                <div class="row">
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <a href="index.php">
+                            <img class="g-width-100--xs g-height-auto--xs" src="img/logo-ecell.png" alt="Ecell Logo">
+                        </a>
+                        <br><br>
+                        <a class="s-header__action-link" href=" https://m.facebook.com/vnitecell/">
+                            <i class="g-padding-r-5--xs ti-facebook"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Facebook</span>
+                        </a>
+                        <a class="s-header__action-link" href="https://twitter.com/ecell_vnit">
+                            <i class="g-padding-r-5--xs ti-twitter"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Twitter</span>
+                        </a>
+                        <a class="s-header__action-link" href=" https://www.instagram.com/ecellvnit/">
+                            <i class="g-padding-r-5--xs ti-instagram"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>
+                        </a>
+                        <br>
+                        <a class="s-header__action-link" href=" https://www.linkedin.com/company/6615520/">
+                            <i class="g-padding-r-5--xs ti-linkedin"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">LinkedIn</span>
+                        </a>
+                        <a class="s-header__action-link" href=" https://www.youtube.com/channel/UC0KNOmODhqLcEpcfN9qEsIQ">
+                            <i class="g-padding-r-5--xs ti-youtube"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">YouTube</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                            <p class="g-color--white">For Association Opportunities Contact:<br><b>contact@ecellvnit.org</b></p>
+                            <p class="g-color--white">For Sponsorship Opportunities Contact:<br><b>shubhamjadhav@ecellvnit.org<b></p>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4 g-text-right--md">
+                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white">Copyright <a class="g-color--primary" href="https://www.ecellvnit.org/" target="_blank">E-cell Vnit</a></p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Copyright -->
+        </footer>
+        <!--========== END FOOTER ==========-->
+
+        <!-- Back To Top -->
+        <a href="javascript:void(0);" class="s-back-to-top js__back-to-top"></a>
+
+        <!--========== JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) ==========-->
+        <!-- Vendor -->
+        <script type="text/javascript" src="vendor/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.migrate.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.smooth-scroll.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.back-to-top.min.js"></script>
+        <script type="text/javascript" src="vendor/scrollbar/jquery.scrollbar.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.parallax.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.wow.min.js"></script>
+
+        <!-- General Components and Settings -->
+        <script type="text/javascript" src="js/global.min.js"></script>
+        <script type="text/javascript" src="js/components/header-sticky.min.js"></script>
+        <script type="text/javascript" src="js/components/scrollbar.min.js"></script>
+        <script type="text/javascript" src="js/components/wow.min.js"></script>
+        <!--========== END JAVASCRIPTS ==========-->
+
+    </body>
+    <!-- End Body -->
+</html>
