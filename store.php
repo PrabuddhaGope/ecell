1ebe816147e41715e24d731ae2b2fff4ee83d8e4
Sagar Bansal <sagarbansal099@gmail.com>
2019-01-30 21:20:27 +0000

    tees3

--- store.php
+++ store.php
@@ -16,7 +16,7 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>STORE | ECELL | VNIT</title>
+        <title>STORE | ECELL VNIT</title>
         <meta name="keywords" content="" />
         <meta name="description" content="Buy brand new official merchandise of the central India's biggest entreprenuship summit Consortium'19 at Ecell VNIT online store.">
         <meta name="author" content="Sagar Bansal">
@@ -32,29 +32,30 @@
 
         <!-- Theme Styles -->
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
-        <link href="css/overlay.css" rel="stylesheet" type="text/css"/>
+        <link href="css/store.css" rel="stylesheet" type="text/css"/>
         <link href="css/global/global.css" rel="stylesheet" type="text/css"/>
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
-        <link rel="apple-touch-icon" href="img/icon.png">
+        <link rel="shortcut icon" href="img/icon1.png" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/icon1.png">
     </head>
     <!-- End Head -->
 
     <!-- Body -->
     <body>
 
+
         <!--========== HEADER ==========-->
 
         <!--========== END HEADER ==========-->
 
         <!--========== PROMO BLOCK ==========-->
-        <div class="g-bg-position--center js__parallax-window" style="background: black 100% 0 no-repeat fixed; height:100vh;">
+        <div class="g-bg-position--center js__parallax-window" style="background:#1d1d1d 100% 0 no-repeat fixed; height:100vh; display:flex; align-items:center">
             <div class="g-container--md g-text-center--xs g-padding-y-40--xs ">
                 <h1 class="g-font-size-20--xs g-font-size-40--sm g-font-size-50--md g-color--white g-letter-spacing--3">STORE | ECELLVNIT</h1>
 
 
-                <div class="store-img g-padding-y-60--xs g-padding-y-60--md">
+                <div class="store-img g-padding-y-60--xs g-padding-y-60--md" style="margin-top:30px">
                     <img class="img-1" src="img/store/1_white-01.png" alt="" >
                     <img class="img-2" src="img/store/1_red-01.png" alt="">
 
@@ -64,11 +65,12 @@
 
 
 
-        <div class="s-promo-block-v3 g-bg-position--center tee g-padding-y-40--xs" style="">
+        <div class="s-promo-block-v3  tee g-padding-y-40--xs" style="">
             <div class="container g-padding-y-125--md g-padding-y-40--xs g-padding-y-40--md" style="top:32%; position: relative;">
                 <div class="g-margin-t-30--xs g-margin-t-0--sm  g-padding-x-20--xs g-margin-t-125--lg g-margin-b-30--xs g-margin-b-40--md">
                     <h1 class="g-font-size-30--xs g-font-size-40--sm g-font-size-60--lg g-color--white g-font-weight--600">CONSORTIUM'19<BR><span style="color:#aaa;">OFFICIAL</span><br><span style="color:#555;"><b>MERCHANDISE</b></span></h1>
-                <div class='pm-button text-uppercase btn-block s-btn s-btn--md s-btn--white-brd s-btn--white-bg g-radius--50 g-padding-x-50--xs g-margin-b-20--xs' STYLE="width:200px"><a href='https://www.payumoney.com/paybypayumoney/#/FEC5F0175992F7F3DE3976C404338B8D'>Buy Now</a></div>
+                <p style="color:#aaa">*Available Sizes: S, M, L, XL, XXL</p>
+                <div class='pm-button text-uppercase btn-block s-btn s-btn--md s-btn--white-brd g-radius--50 g-padding-x-20--xs g-margin-b-20--xs tee-button' STYLE=""><a href='https://www.payumoney.com/paybypayumoney/#/FEC5F0175992F7F3DE3976C404338B8D'>Buy Now</a></div>
 
                 </div>
 
@@ -93,9 +95,18 @@
             <div class="container g-padding-y-20--xs">
                 <div class="row">
                     <div class="col-sm-12 col-md-4 col-lg-4">
-                        <a href="index.php">
-                            <img class="g-width-100--xs g-height-auto--xs" src="img/logo-ecell.png" alt="Ecell Logo">
-                        </a>
+                        <div style="display: flex">
+
+
+                                    <a href="http://pnaf.in">
+                                        <!--<img class=" g-height-auto--xs" src="static/img/consologo.png" alt="StartUp Conclave" height="45" width="45">-->
+                                        <img class="img-responsive g-padding-x-0--xs" style="height: 45px" src="img/logo-ecell.png" alt="Ecell">
+                                    </a>
+
+                                    <a href="/" style="margin-left: 20px;">
+                                        <img class="img-responsive g-height-auto--xs" style="height: 45px" src="img/icon1.png" alt="Ecell Logo">
+                                    </a>
+                                </div>
                         <br><br>
                         <a class="s-header__action-link" href=" https://m.facebook.com/vnitecell/">
                             <i class="g-padding-r-5--xs ti-facebook"></i>
@@ -120,11 +131,11 @@
                         </a>
                     </div>
                     <div class="col-sm-12 col-md-4 col-lg-4">
-                            <p class="g-color--white">For Association Opportunities Contact:<br><b>contact@ecellvnit.org</b></p>
-                            <p class="g-color--white">For Sponsorship Opportunities Contact:<br><b>shubhamjadhav@ecellvnit.org<b></p>
+                            <p class="g-color--white">Contact:<br><b>ROHIT: </b> 89836 82793<BR><b>AYUSHI: </b>88303 14193</p>
+                            <p class="g-color--white">Buy at: <b>store.ecellvnit.org<b></p>
                     </div>
                     <div class="col-sm-12 col-md-4 col-lg-4 g-text-right--md">
-                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white">Copyright <a class="g-color--primary" href="https://www.ecellvnit.org/" target="_blank">E-cell Vnit</a></p>
+                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white">Copyright <a class="g-color--red" href="https://www.ecellvnit.org/" target="_blank">E-cell Vnit</a></p>
                     </div>
                 </div>
             </div>
@@ -151,6 +162,7 @@
         <script type="text/javascript" src="js/components/header-sticky.min.js"></script>
         <script type="text/javascript" src="js/components/scrollbar.min.js"></script>
         <script type="text/javascript" src="js/components/wow.min.js"></script>
+
         <!--========== END JAVASCRIPTS ==========-->
 
     </body>
