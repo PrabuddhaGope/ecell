282c0121d98ad530ab2871a71fdfad63a18b2b50
Sagar Bansal <sagarbansal099@gmail.com>
2019-01-30 18:23:19 +0000

    tee3

--- store.php
+++ store.php
@@ -67,7 +67,7 @@
         <div class="s-promo-block-v3 g-bg-position--center tee g-padding-y-40--xs" style="">
             <div class="container g-padding-y-125--md g-padding-y-40--xs g-padding-y-40--md" style="top:32%; position: relative;">
                 <div class="g-margin-t-30--xs g-margin-t-0--sm  g-padding-x-20--xs g-margin-t-125--lg g-margin-b-30--xs g-margin-b-40--md">
-                    <h1 class="g-font-size-30--xs g-font-size-40--sm g-font-size-60--lg g-color--white g-font-weight--600">CONSORTIUM'19<BR><span style="color:#aaa;">OFFICIAL</span><br><span style="color:#555;"><b>MERCHANDIES</b></span></h1>
+                    <h1 class="g-font-size-30--xs g-font-size-40--sm g-font-size-60--lg g-color--white g-font-weight--600">CONSORTIUM'19<BR><span style="color:#aaa;">OFFICIAL</span><br><span style="color:#555;"><b>MERCHANDISE</b></span></h1>
                 <div class='pm-button text-uppercase btn-block s-btn s-btn--md s-btn--white-brd s-btn--white-bg g-radius--50 g-padding-x-50--xs g-margin-b-20--xs' STYLE="width:200px"><a href='https://www.payumoney.com/paybypayumoney/#/FEC5F0175992F7F3DE3976C404338B8D'>Buy Now</a></div>
 
                 </div>
